59e9a280c0fcebc00a834718041d34156c22e67e
SMJSGaming <sjoerd.t@live.nl>
2022-09-12 20:21:11 +0200

    Added db info write support

--- src/structures/DBInfo.ts
+++ src/structures/DBInfo.ts
@@ -1,8 +1,12 @@
+import { Type } from "../enums/Type";
 import { BinReader } from "../utils/BinReader";
+import { BinWriter } from "../utils/BinWriter";
 
 export class DBInfo {
 
     public static readonly SPEED_OF_LIGHT = 299792458;
+
+    public static readonly VERSION = 1;
 
     public readonly version: number;
     
@@ -12,35 +16,48 @@ export class DBInfo {
 
     public readonly lastUpdate: Date;
 
-    constructor(reader: BinReader) {
-        const checkpoint = reader.addCheckpoint();
+    constructor(reader: BinReader);
+    constructor(writer: BinWriter, x64: boolean, creation: Date);
+    constructor(arg0: BinReader | BinWriter, arg1?: boolean, arg2?: Date) {
 
-        try {
-            if (reader.readInteger(false) == DBInfo.SPEED_OF_LIGHT) {
-                this.version = reader.readInteger(false);
-                this.x64 = reader.readBool(false);
+        if (arg0 instanceof BinReader) {
+            const checkpoint = arg0.addCheckpoint();
 
-                const creation = reader.readLong(false);
-                const lastUpdate = reader.readLong(false);
+            try {
+                if (arg0.readInteger(false) == DBInfo.SPEED_OF_LIGHT) {
+                    this.version = arg0.readInteger(false);
+                    this.x64 = arg0.readBool(false);
 
-                if (creation >= 0 && creation < Number.MAX_SAFE_INTEGER) {
-                    this.creation = new Date(Number(creation));
+                    const creation = arg0.readLong(false);
+                    const lastUpdate = arg0.readLong(false);
+
+                    if (creation >= 0 && creation < Number.MAX_SAFE_INTEGER) {
+                        this.creation = new Date(Number(creation));
+                    } else {
+                        throw new Error("Invalid creation date");
+                    }
+
+                    if (lastUpdate >= 0 && lastUpdate < Number.MAX_SAFE_INTEGER) {
+                        this.lastUpdate = new Date(Number(lastUpdate));
+                    } else {
+                        throw new Error("Invalid last update date");
+                    }
                 } else {
-                    throw new Error("Invalid creation date");
+                    throw new Error("Sorry, your binary file has to travel at the speed of light to go interstellar");
                 }
+            } catch(error) {
+                arg0.rollbackTo(checkpoint);
 
-                if (lastUpdate >= 0 && lastUpdate < Number.MAX_SAFE_INTEGER) {
-                    this.lastUpdate = new Date(Number(lastUpdate));
-                } else {
-                    throw new Error("Invalid last update date");
-                }
-            } else {
-                throw new Error("Sorry, your binary file has to travel at the speed of light to go interstellar");
+                throw error;
             }
-        } catch(error) {
-            reader.rollbackTo(checkpoint);
-
-            throw error;
+        } else if (typeof arg1 != "undefined" && arg2) {
+            arg0.writeNumberType(Type.INTEGER, DBInfo.SPEED_OF_LIGHT, false);
+            arg0.writeNumberType(Type.INTEGER, this.version = DBInfo.VERSION, false);
+            arg0.writeBool(this.x64 = arg1, false);
+            arg0.writeNumberType(Type.LONG, (this.creation = arg2).getTime(), false);
+            arg0.writeNumberType(Type.LONG, (this.lastUpdate = new Date()).getTime(), false);
+        } else {
+            throw new Error("Invalid constructor arguments.");
         }
     }
 }
